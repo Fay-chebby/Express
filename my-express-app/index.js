87fd4641b8070d721734b9de3d3acb1069005bee
Fay-chebby <chebetf034@gmail.com>
2025-06-05 16:14:50 +0300

    working on routes

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,9 +4,17 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world , how are you today");
 });
 
+app.use((req, res, next) => {
+  console.log("${req.method} ${req.url");
+  next();
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
