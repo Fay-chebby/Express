7e0dd3ff96f3ca5313018ef30cd5f325cb4f612e
Fay-chebby <chebetf034@gmail.com>
2025-06-11 14:14:22 +0300

    adding comments

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -1,67 +1,67 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const PORT = 3000;
-app.use(express.json());
+// const PORT = 3000;
+// app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world , how are you today");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello world , how are you today");
+// });
 
-app.use((req, res, next, err) => {
-  console.log("${req.method} ${req.url");
-  next();
-});
+// app.use((req, res, next, err) => {
+//   console.log("${req.method} ${req.url");
+//   next();
+// });
 
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
+// app.get("/about", (req, res) => {
+//   res.send("About page");
+// });
 
-app.post("/contact", (req, res) => {
-  res.send("Contact page");
-});
-// Route params
-app.get("/user/:id", (req, res) => {
-  res.send("User ID: ${req.params.id}");
-});
+// app.post("/contact", (req, res) => {
+//   res.send("Contact page");
+// });
+// // Route params
+// app.get("/user/:id", (req, res) => {
+//   res.send("User ID: ${req.params.id}");
+// });
 
-//Query params
-app.get("/search", (req, res) => {
-  res.send("Search query: ${req.query.q}");
-});
+// //Query params
+// app.get("/search", (req, res) => {
+//   res.send("Search query: ${req.query.q}");
+// });
 
-//CRUD create,update,delete
-let users = [{ id: 1, name: "Faith" }];
-//create
-app.post("/users", (req, res) => {
-  const newUser = { id: User.length + 1, name: req.body.name };
+// //CRUD create,update,delete
+// let users = [{ id: 1, name: "Faith" }];
+// //create
+// app.post("/users", (req, res) => {
+//   const newUser = { id: User.length + 1, name: req.body.name };
 
-  user.push(newUser);
-  res.status(201).json(newUser);
-});
+//   user.push(newUser);
+//   res.status(201).json(newUser);
+// });
 
-//get
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-// getting a single user
-app.get("/users/:id", (req, res) => {
-  const user = users.find((u) => u.id === Number(req.params.id));
-  res.json(users);
-});
+// //get
+// app.get("/users", (req, res) => {
+//   res.json(users);
+// });
+// // getting a single user
+// app.get("/users/:id", (req, res) => {
+//   const user = users.find((u) => u.id === Number(req.params.id));
+//   res.json(users);
+// });
 
-//update
-app.put("/users/:id", (req, res) => {
-  const user = user.find((u) => u.id === Number(req.params.id));
-  user.name = req.body.name;
-  res.json(user);
-});
-// delete
-app.delete("/users/:id", (req, res) => {
-  user = user.filter((u) => u.id != req.params.id);
-  res.status(204).send();
-});
+// //update
+// app.put("/users/:id", (req, res) => {
+//   const user = user.find((u) => u.id === Number(req.params.id));
+//   user.name = req.body.name;
+//   res.json(user);
+// });
+// // delete
+// app.delete("/users/:id", (req, res) => {
+//   user = user.filter((u) => u.id != req.params.id);
+//   res.status(204).send();
+// });
 
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+// app.listen(PORT, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
--- my-express-app/server.js
+++ my-express-app/server.js
@@ -1,5 +1,21 @@
+//import
 const express = require("express");
 const mongoose = require("mongoose");
+//express and middleware
 const app = express();
 app.use(express.json());
-const mongoUri = " mongodb://localhost:27017/";
+//constants
+const mongoUri = " mongodb://localhost:27017/taskdb";
+const PORT = 3000;
+//mongo db connection
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.error("mongodb connection error:", err));
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:3000");
+});
