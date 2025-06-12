5648fff45252033a0360c79e13d2654996239b3f
Fay-chebby <chebetf034@gmail.com>
2025-06-12 21:50:29 +0300

    data

---
+++ my-express-app/routes.js
@@ -0,0 +1,25 @@
+const Task = require("./Task");
+const express = require("express");
+const router = express.Router();
+// const app = express();
+//inputing data
+router.post("/task", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+// getting data
+router.get("/task", async (req, res) => {
+  try {
+    const task = await Task.find();
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
--- my-express-app/server.js
+++ my-express-app/server.js
@@ -1,11 +1,12 @@
 //import
 const express = require("express");
 const mongoose = require("mongoose");
+const taskRoutes = require("./routes");
 //express and middleware
 const app = express();
 app.use(express.json());
 //constants
-const mongoUri = " mongodb://localhost:27017/taskdb";
+const mongoUri = "mongodb://localhost:27017/taskdb";
 const PORT = 3000;
 //mongo db connection
 mongoose
@@ -13,8 +14,11 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("connected to mongodb"))
+  .then(() => console.log(" connected to mongodb"))
   .catch((err) => console.error("mongodb connection error:", err));
+
+// use task routes
+app.use("/", taskRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
